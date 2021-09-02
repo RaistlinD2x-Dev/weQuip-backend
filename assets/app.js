06d3d5a2c23d7aaf52ab703bc00fb8c216f781c3
RaistlinD2x-Dev <RaistlinD2x.dev@outlook.com>
2021-09-02 07:40:11 -0500

    testing connection to mongodb

--- assets/app.js
+++ assets/app.js
@@ -1,5 +1,5 @@
-// const axios = require('axios')
-// const url = 'http://checkip.amazonaws.com/';
+const clientPromise = require('./mongodb-client');
+
 let response;
 
 /**
@@ -7,27 +7,15 @@ let response;
  * Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
  * @param {Object} event - API Gateway Lambda Proxy Input Format
  *
- * Context doc: https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-context.html 
+ * Context doc: https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-context.html
  * @param {Object} context
  *
  * Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
  * @returns {Object} object - API Gateway Lambda Proxy Output Format
- * 
+ *
  */
 exports.lambdaHandler = async (event, context) => {
-    try {
-        // const ret = await axios(url);
-        response = {
-            'statusCode': 200,
-            'body': JSON.stringify({
-                message: 'hello world',
-                // location: ret.data.trim()
-            })
-        }
-    } catch (err) {
-        console.log(err);
-        return err;
-    }
+  const client = await clientPromise;
 
-    return response
+  return client.db().databaseName;
 };
---
+++ assets/mongodb-client.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const { MongoClient } = require('mongodb');
+
+const MONGODB_URI = async () => {
+  await ssm.putParameter(params).promise();
+  const data = await ssm
+    .getParameters({
+      Connect: ['MONGODB_URI'],
+    })
+    .promise();
+
+  return data;
+};
+
+const client = new MongoClient(MONGODB_URI(), {
+  useNewUrlParsers: true,
+  useUnifiedTopology: true,
+});
+
+module.exports = client.connect();
