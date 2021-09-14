2425288a3c20029553474bf69eba914ddab3f99c
Jesse Richey <RaistlinD2x.dev@outlook.com>
2021-09-14 15:09:43 -0500

    setup get locations endpoint for locations data in frontend

---
+++ get-locations/locationSchema.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+
+// create schema validation for mongoose model
+const locationSchema = new mongoose.Schema(
+  {
+    address: {
+      type: Object,
+      address1: {
+        type: String
+      },
+      address2: {
+        type: String
+      },
+      city: {
+        type: String
+      },
+      state: {
+        type: String
+      },
+      postalCode: {
+        type: String
+      },
+      country: {
+        type: String
+      }
+    },
+    locationAcronym: {
+      type: String
+    }
+  },
+  { timestamps: true },
+);
+
+// create mongoose model from Schema
+const Location = mongoose.model('Location', locationSchema);
+
+module.exports = Location;
